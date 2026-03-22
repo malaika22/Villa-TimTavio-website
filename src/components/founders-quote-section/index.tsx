915af396463fa8c7c6e6434b3778af60105b5504
malaika22 <malaikaafridi22@gmail.com>
2026-03-22 21:08:09 +0500

    added founders quote section

---
+++ src/components/founders-quote-section/index.tsx
@@ -0,0 +1,12 @@
+export const FoundersQuoteSection = () => {
+  return (
+    <div className="h-[705px] bg-[#E3E0DA] flex flex-col items-center justify-center gap-[72px]">
+      <div className="bg-[#8C7261] w-[1px] h-[48px]"></div>
+      <h5 className="text-[38px] font-light italic max-w-[680px] mx-auto text-center">
+        &quot;We built this place because the world already has enough luxury. What it lacks is
+        quiet.&quot;
+      </h5>
+      <p className="text-[#797168] text-sm">The Founders · Casa TimTavio</p>
+    </div>
+  );
+};
