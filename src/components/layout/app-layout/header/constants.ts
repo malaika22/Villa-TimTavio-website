5b4ba81926ef61c0896549539fb9c66a57521cd7
malaika22 <malaikaafridi22@gmail.com>
2026-03-22 21:10:46 +0500

    added layout header

---
+++ src/components/layout/app-layout/header/constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    label: "THE ESTATE",
+    href: "#estate",
+  },
+  {
+    label: "EXPERIENCES",
+    href: "#experiences",
+  },
+  {
+    label: "THE CIRCLE",
+    href: "#the-circle",
+  },
+  {
+    label: "CONTACT",
+    href: "#contact",
+  },
+];
